d4d4a21b2e54a63df615a4af0daf03ecd1ef60d3
hackerboy09 <isc1930319@gmail.com>
2023-03-03 10:08:18 -0600

    Estructura de calculadora

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,109 @@
+import './App.css'
+
 function App() {
   return (
     <div>
     <h1>Calculator</h1>
-    <table style={{width: '400px'}}>
+    <table>
+      {/*First row */}
       <tr>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
+        <td colSpan={4}>RESULT</td>
+
       </tr>
+      {/*Second row */}
       <tr>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
+        <td> 
+        <button
+        type= "button"
+         className='btn btn-primary'> C </button> 
+        </td>
+        <td>
+        <button
+        type= "button" 
+        className='btn btn-primary'> / </button> 
+        </td>
+        <td> 
+        <button
+        type= "button" 
+        className='btn btn-primary'> * </button> 
+        </td>
+        <td> 
+        <button
+        type= "button" 
+        className='btn btn-primary'> - </button> 
+        </td>
       </tr>
+      {/*Third row */}
       <tr>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
+      <td> 
+        <button
+        type= "button"
+         className='btn btn-primary'> 7 </button> 
+        </td>
+        <td> 
+        <button
+        type= "button"
+         className='btn btn-primary'> 8 </button> 
+        </td>
+        <td> 
+        <button
+        type= "button"
+         className='btn btn-primary'> 9 </button> 
+        </td>
+        <td rowSpan={2}><button
+        type= "button"
+         className='btn btn-primary'> + </button>
+          </td>
       </tr>
+     {/*Fourth row */}
       <tr>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
+      <td> 
+        <button
+        type= "button"
+         className='btn btn-primary'> 4 </button> 
+        </td>
+        <td> 
+        <button
+        type= "button"
+         className='btn btn-primary'> 5 </button> 
+        </td>
+        <td> 
+        <button
+        type= "button"
+         className='btn btn-primary'> 6 </button> 
+        </td>
       </tr>
+       {/*Fifth row */}
       <tr>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
+      <td> 
+        <button
+        type= "button"
+         className='btn btn-primary'> 1 </button> 
+        </td>
+        <td> 
+        <button
+        type= "button"
+         className='btn btn-primary'> 2 </button> 
+        </td>
+        <td> 
+        <button
+        type= "button"
+         className='btn btn-primary'> 3 </button> 
+        </td>
+        <td rowSpan={2}><button
+        type= "button"
+         className='btn btn-primary'> = </button></td>
       </tr>
+       {/*Sixth row */}
       <tr>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
+        <td colSpan={2}><button
+        type= "button"
+         className='btn btn-primary'> 0 </button></td>
+        <td> 
+        <button
+        type= "button"
+         className='btn btn-primary'> . </button> 
+        </td>
       </tr>
     </table>
     </div>
